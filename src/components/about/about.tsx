81eeef9a32d55c6247adb7aab386a70292c5e921
Maksim P <email@example.comkfc.kurochka@gmail.com>
2025-06-16 01:14:26 +0600

    start

---
+++ src/components/about/about.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { useState } from 'react';
+import styles from './about.module.css';
+import Image from 'next/image';
+import { svg01 } from '../icons/icons';
+
+export default function About() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.left}>{svg01}</div>
+      <div className={styles.right}>
+        <Image
+          className={styles.img}
+          src="/img/lampa.png"
+          alt="Logo"
+          width={500}
+          height={500}
+          priority
+          style={{ width: 'auto', height: 'auto', marginLeft: 'auto' }}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,71 @@
+'use client';
+import { useState } from 'react';
 import styles from './header.module.css';
+import Image from 'next/image';
+import { menu, profile, basket, search, ellipse } from '../icons/icons';
+import About from '../about/about';
 
 export default function Header() {
-  return <div></div>;
+  const [pick, setPick] = useState('main');
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.up}>
+        <div className={styles.fon}>
+          {
+            <Image
+              className={styles.img}
+              src="/img/logo.png"
+              alt="Logo"
+              width={118}
+              height={56}
+              priority
+            />
+          }
+        </div>
+        <div className={styles.contacts}>
+          <div className={styles.lampa} data-pick={pick}>
+            {ellipse}
+          </div>
+          <div className={styles.menu}>
+            <span
+              onClick={() => setPick('main')}
+              className={pick == 'main' ? styles.menuPick : ''}
+            >
+              Главная
+            </span>
+            <span
+              onClick={() => setPick('catalog')}
+              className={pick == 'catalog' ? styles.menuPick : ''}
+            >
+              Каталог
+            </span>
+            <span
+              onClick={() => setPick('basket')}
+              className={pick == 'basket' ? styles.menuPick : ''}
+            >
+              Корзина
+            </span>
+          </div>
+          <a href="tel:+73812387535" className={styles.tel}>
+            +7 (3812) 387-535
+          </a>
+        </div>
+      </div>
+      <div className={styles.down}>
+        <div className={styles.flex}>
+          <div className={styles.flex}>{menu}</div>
+          <div className={styles.input}>
+            <input placeholder="Поиск" type="text" />
+            <div className={styles.svg}>{search}</div>
+          </div>
+          <div className={styles.flex}>
+            {profile}
+            {basket}
+          </div>
+        </div>
+        <About />
+      </div>
+    </div>
+  );
 }
